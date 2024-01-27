a542be18ac8f4b1b055aa4aad6610c3cdcf8c260
chitoku <chitoku@gmail.com>
2024-01-27 05:42:00 -0800

    Create scraper.py

---
+++ scraper.py
@@ -0,0 +1,25 @@
+from lxml import html
+from lxml import etree
+from lxml.etree import tostring
+import requests
+
+def prettyprint(element, **kwargs):
+  xml = etree.tostring(element, **kwargs)
+  return xml.decode()
+
+# Make a GET request to the website
+page = requests.get('https://zero.estate/category/zero/chugoku/okayama/')
+hp = etree.HTMLParser(encoding='utf-8')
+tree = html.fromstring(page.content, parser=hp)
+
+# Use XPath to select the desired element(s)
+#items = tree.xpath('//div[class="kanren "]/dl/dd/h3/text()')
+properties = tree.xpath('//h3/a')
+print(prettyprint(properties[0]))
+
+dates = tree.xpath('//div[@class="blog_info"]/p/i')
+print(prettyprint(dates[0]))
+
+filename = 'okayama.html'
+with open(filename, 'w') as modified:
+  modified.write('<dl><dd>' + prettyprint(dates[0]) + ' ' + prettyprint(properties[0]) + '</dd></dl>\n')
